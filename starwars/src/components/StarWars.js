8a0883fcb71aa1a6ea9eaa7938768cee13ebc478
Jeff15113 <itsmejeffery@gmail.com>
2019-01-11 17:29:09 -0500

    removed console log and added key property

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -4,9 +4,8 @@ import './StarWars.css'
 const StarWars = (props) => {
     return (
         <div className='starwars'>
-            {console.log(props)}
             {props.characterList.map((x, index) => (
-                <StarCard name={x.name} birthyear={x.birth_year} haircolor={x.hair_color} height={x.height} eyecolor={x.eye_color} gender={x.gender} mass={x.mass} />
+                <StarCard key={index} name={x.name} birthyear={x.birth_year} haircolor={x.hair_color} height={x.height} eyecolor={x.eye_color} gender={x.gender} mass={x.mass} />
             ))}
 
         </div>
